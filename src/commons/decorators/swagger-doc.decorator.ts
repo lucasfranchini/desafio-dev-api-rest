2b2fbd5f4468893795b2949e84ef0af478183c63
lucasfranchini <lucasfranchini.a@gmail.com>
2025-06-22 21:25:40 -0300

    install axios for http calls

--- src/commons/decorators/swagger-doc.decorator.ts
+++ src/commons/decorators/swagger-doc.decorator.ts
@@ -1,6 +1,6 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { ErrorsMessage, ErrorsSource } from '../errors/enums';
+import { ErrorsMessage } from '../errors/enums';
 
 export function SwaggerDocDecorator(summary: string) {
   return applyDecorators(
@@ -10,14 +10,6 @@ export function SwaggerDocDecorator(summary: string) {
       description: 'Erro inesperado ao buscar transações no banco de dados',
       example: {
         message: ErrorsMessage.DEFAULT,
-      },
-    }),
-    ApiResponse({
-      status: 401,
-      description: 'cliente não autorizado',
-      example: {
-        message: ErrorsMessage.UNAUTHORIZED_CLIENT,
-        source: ErrorsSource.AUTHENTICATION_INTERCEPTOR,
       },
     }),
   );
